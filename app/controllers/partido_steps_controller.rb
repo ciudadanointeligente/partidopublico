c3bc6df2fa0b33f13756e3773b742078774f9df6
Jordi Bari <jbari@ciudadanointeligente.org>
2016-05-25 19:30:58 -0400

    wip personas datagrid in steps

--- app/controllers/partido_steps_controller.rb
+++ app/controllers/partido_steps_controller.rb
@@ -1,47 +1,52 @@
 class PartidoStepsController < ApplicationController
     include Wicked::Wizard
     helper  :all
-    
+
     before_action :set_partido
-        
-    
-        
-    steps   :datos_basicos, 
-            :normas_internas, 
+
+
+
+    steps   :datos_basicos, :personas,
+            :normas_internas,
             :regiones, :sedes, :num_afiliados, :tramites, :representantes, :autoridades,
             :postulacion_popular, :organos_internos, :postulacion_interna, :agenda_presidente, :actividades_publicas,
-            
+
             :candidatos, :acuerdos_organos, :resultados_elecciones_internas,
-            
+
             :entidades_participadas, :pactos_electorales,
-            
+
             :linea_denuncia, :sanciones
-    
+
     def show
         # @user = current_user
         # @partido = Partido.find_by_user_id(current_user.id)
         puts "----------------->  Show::"+step.to_s
         case step
         when :datos_basicos
-            
+
+        when :personas
+          @grid = PersonasGrid.new(params[:personas_grid]) do |scope|
+            scope.page(params[:page])
+          end
+
         when :normas_internas
-        
+
         when :regiones
-            
+
         when :sedes
             @partido.regions.each do |r|
                 if @partido.sedes.find_by_region(r.to_s).blank?
                     @partido.sedes << Sede.new(region:r)
                 end
             end
-        
+
         when :num_afiliados
             @partido.regions.each do |r|
                 if @partido.afiliacions.find_by_region(r.to_s).blank?
                     @partido.afiliacions << Afiliacion.new(region:r)
                 end
             end
-        
+
         when :tramites
             @partido.tramites.each do |tramite|
                 tramite.requisitos.each do |requisito|
@@ -49,44 +54,44 @@ class PartidoStepsController < ApplicationController
                 tramite.procedimientos.each do |procedimiento|
                 end
             end
-        
+
         end
         render_wizard
     end
-    
+
     def update
-        
+
         # puts params[:partido]
         # @partido = Partido.find_by_user_id(current_user.id)
         puts "----------------->  Update::"+step.to_s
         case step
         # when :datos_basicos
         #     @partido.update_attributes(partido_params)
-        
+
         when :normas_internas
             @partido.marco_interno.update(marco_interno_params)
-        
+
         # when :regiones
         #     @partido.update_attributes(partido_params)
-        
+
         # when :sedes
         #     @partido.update_attributes(partido_params)
-        
+
         # when :num_afiliados
         #     @partido.update_attributes(partido_params)
-        
+
         # when :tramites
         #     @partido.update_attributes(partido_params)
-                
+
         # when :representantes
         #     @partido.update_attributes(partido_params)
-        
+
         # when :autoridades
         #     @partido.update_attributes(partido_params)
-                
+
         # when :postulacion_popular
         #     @partido.update_attributes(partido_params)
-                
+
         else
             @partido.update_attributes(partido_params)
         end
@@ -97,13 +102,13 @@ class PartidoStepsController < ApplicationController
             render_wizard @partido
         end
     end
-  
+
     private
         # Use callbacks to share common setup or constraints between actions.
         def set_partido
           @partido = Partido.find params[:partido_id]
         end
-    
+
         # Never trust parameters from the scary internet, only allow the white list through.
         def partido_params
           params.require(:partido).permit(:nombre, :sigla, :lema, :fecha_fundacion, :texto, :logo,
@@ -141,7 +146,7 @@ class PartidoStepsController < ApplicationController
                                                     region_ids: []
             )
         end
-        
+
         def marco_interno_params
           params.require(:marco_interno).permit(:partido_id, documentos_attributes: [:id, :descripcion, :archivo, :_destroy])
         end
--- app/helpers/partido_steps_helper.rb
+++ app/helpers/partido_steps_helper.rb
@@ -1,29 +1,29 @@
 module PartidoStepsHelper
-  
+
     def main_menu
       step_categories = ["INICIO", "NORMAS INTERNAS Y PENSAMIENTO", "PRESENCIA NACIONAL", "CÓMO PARTICIPAR", "ÚLTIMAS DECISIONES", "VÍNCULOS E INTERESES", "DENUNCIAS Y SANCIONES", "FINANZAS"]
     end
-    
+
     def sub_menu
       step_categories = main_menu
-      step_organisation = { step_categories[0] => ["datos_basicos"], 
-                            step_categories[1] => ["normas_internas"], 
+      step_organisation = { step_categories[0] => ["datos_basicos", "personas"], 
+                            step_categories[1] => ["normas_internas"],
                             step_categories[2] => ["regiones", "sedes", "num_afiliados", "tramites", "representantes", "autoridades" ],
                             step_categories[3] => ["postulacion_popular", "organos_internos", "postulacion_interna", "agenda_presidente", "actividades_publicas" ],
                             step_categories[4] => ["candidatos", "acuerdos_organos", "resultados_elecciones_internas"],
-                            step_categories[5] => ["entidades_participadas", "pactos_electorales"], 
+                            step_categories[5] => ["entidades_participadas", "pactos_electorales"],
                             step_categories[6] => ["linea_denuncia", "sanciones"],
                             step_categories[7] => ["a","b"]}
     end
-  
+
     def substeps_progress_bar
       step_organisation = sub_menu
-                            
+
       key = step_organisation.select{|k,v| v.include? step.to_s}.keys.first
       values = step_organisation.select{|k,v| v.include? step.to_s}.values.first
-      
+
       content_tag(:section, class: "content") do
-        content_tag(:div, class: "navigator") do        
+        content_tag(:div, class: "navigator") do
           content_tag(:ol, class: "steps_list") do
             values.each do |sub_step|
               class_str = "unfinished"
@@ -31,22 +31,22 @@ module PartidoStepsHelper
               concat(
                 content_tag(:li, class: class_str) do
                   link_to (sub_step), wizard_path(sub_step)
-                end 
-              )   
-            end 
+                end
+              )
+            end
           end
         end
       end
     end
-    
-    
+
+
     def steps_progress_bar
       step_organisation = sub_menu
       step_categories = main_menu
-      
+
       key = step_organisation.select{|k,v| v.include? step.to_s}.keys.first
       values = step_organisation.select{|k,v| v.include? step.to_s}.values.first
-      
+
       content_tag(:section, class: "content") do
         content_tag(:div, class: "navigator") do
           content_tag(:ol, class: "main_steps_list") do
@@ -56,14 +56,14 @@ module PartidoStepsHelper
               concat(
                 content_tag(:li, class: class_str) do
                   link_to (main_step), wizard_path(step_organisation[main_step].first)
-                end 
-              )   
-            end 
+                end
+              )
+            end
           end
         end
       end
-    end 
-    
+    end
+
     def tutorial_progress_bar
       content_tag(:section, class: "content") do
         content_tag(:div, class: "navigator") do
@@ -75,12 +75,12 @@ module PartidoStepsHelper
               concat(
                 content_tag(:li, class: class_str) do
                   link_to (every_step), wizard_path(every_step)
-                end 
-              )   
-            end 
-          end 
+                end
+              )
+            end
+          end
         end
       end
     end
-    
+
 end
