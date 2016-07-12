f0f6ba19e0b1f363ecad07595de03819b1f3c05e
Lissteilor <lissteilor@Danielas-iMac.local>
2016-07-12 13:47:01 -0300

    Página partidos políticos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
     def index
        @partidos = Partido.all
-       render layout: 'transparencia'
+       render layout: 'partidos-politicos'
     end
 
 
