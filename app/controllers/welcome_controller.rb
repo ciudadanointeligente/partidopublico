085f09cac0564dbc782aa2ccba393c27cd3db70f
Lissteilor <lissteilor@Danielas-iMac.local>
2016-07-12 13:18:58 -0300

    Página transparencia

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
     def index
        @partidos = Partido.all
-       render layout: 'lanzamiento'
+       render layout: 'transparencia'
     end
 
 
