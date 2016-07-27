d645a4368f75b19227ec913ff04ce25df877bf54
Jose Damian Garrido Munoz <damian.garrido.munoz@gmail.com>
2016-07-27 17:24:27 -0400

    fix test

--- spec/views/partidos/show.html.erb_spec.rb
+++ spec/views/partidos/show.html.erb_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "partidos/show", type: :view do
   before(:each) do
     @partido = FactoryGirl.create(:partido)
+    @datos_sedes = []
+    @datos_region = []
+    @datos_cargos = []
   end
 
   it "renders attributes in <p>" do
