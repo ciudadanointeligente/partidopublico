9d0a35dc2ac0a5981cff24952c4c2f81c89f5f79
jbci <jbari@ciudadanoineligente.org>
2017-08-10 19:47:58 -0400

    fix provicias fk

--- db/migrate/20160413115409_create_comunas.rb
+++ db/migrate/20160413115409_create_comunas.rb
@@ -1,7 +1,7 @@
 class CreateComunas < ActiveRecord::Migration
   def change
     create_table :comunas do |t|
-      t.references :region, index: true, foreign_key: true
+      t.references :provincia, index: true, foreign_key: true
       t.string :nombre
 
       t.timestamps null: false
